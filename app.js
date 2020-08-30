e163618680900784b0d89daef72646685ad0215b
Kei Hendrikz <keihendrikz@Keis-MBP.lan>
2020-08-30 11:40:39 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+var items = ["Brush teeth", "Make breakfast"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render('lists', {kindOfDay: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
